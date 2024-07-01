42de472ed075dc29d63d8c815832dc9f8598e75a
Nikita A. Dobryukha <ndobryukha@t1-consulting.ru>
2024-07-01 14:03:48 +0400

    implemented homework 3

--- src/test/java/ru/ndobryukha/otus/highload/demo/DemoApplicationTests.java
+++ src/test/java/ru/ndobryukha/otus/highload/demo/DemoApplicationTests.java
@@ -3,6 +3,10 @@ package ru.ndobryukha.otus.highload.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
